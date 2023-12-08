e8235f2fb6e1a40c0b37a8b2a2bb2bece6b6cc34
Saee <saee_saadat@outlook.com>
2023-12-08 23:50:07 +0330

    erased submissions table

--- backend/data/erase_second_third.py
+++ backend/data/erase_second_third.py
@@ -7,6 +7,17 @@ LIMIT = 150
 def main():
     connection = sqlite3.connect("database.sqlite")
     cursor = connection.cursor()
+
+    table_query = """
+        create table if not exists erased_submissions (
+            row_num integer not null,
+            username text not null,
+            answers text not null,
+            primary key (row_num, username)
+        );
+    """
+    cursor.execute(table_query)
+    connection.commit()
     query = "select row_num, username, answers from submissions;"
     cursor.execute(query)
     result = cursor.fetchall()
@@ -15,6 +26,7 @@ def main():
         answers = answers.split(',')
         if answers[1] == '7' or answers[2] == '7':
             print(f"Row {row_num} by {username} is being pruned. prv answers: {answers}")
+            cursor.execute("insert into erased_submissions values (?, ?, ?)", (row_num, username, ','.join(answers)))
             cursor.execute("delete from submissions where row_num = ? and username = ?", (row_num, username))
             counter += 1
         if counter >= LIMIT:
